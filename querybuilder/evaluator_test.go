33ac413cc66c135a4874770fa73f93d81632ed5c
Leandro Nunes <leandronunes.dev@gmail.com>
2019-12-18 11:12:16 -0300

    add test for Match

--- querybuilder/evaluator_test.go
+++ querybuilder/evaluator_test.go
@@ -1,43 +1,37 @@
 package querybuilder
 
-import (
-	//"encoding/json"
-	"testing"
-)
+import "testing"
 
-var rulestr = `{
+var rulesetStr = `{
   "condition": "AND",
-  "rules": [{
-    "id": "Decimal_Question",
-    "field": "Decimal_Question",
-    "type": "double",
-    "input": "text",
-    "operator": "equal",
-    "value": "1.2"
-  }, {
-    "condition": "AND",
-    "rules": [{
-      "id": "Date_Question",
-      "field": "Date_Question",
-      "type": "date",
-      "input": "text",
-      "operator": "greater",
-      "value": "2016-07-19"
-    }, {
-      "id": "Yes_No_Question",
-      "field": "Yes_No_Question",
-      "type": "boolean",
-      "input": "select",
-      "operator": "equal",
-      "value": "true"
-    }]
-  }]
-}`
+  "rules": [
+		{"id": "float_equal","field": "float_equal","type": "double","input": "text","operator": "equal","value": 1.2},
+		{"id": "float_greater","field": "float_greater","type": "double","input": "text","operator": "greater","value": 7.5},
+		{"condition": "OR","rules": [
+			{"id": "int_greater","field": "int_greater","type": "integer","input": "text","operator": "greater","value": 2},
+			{"id": "int_equal","field": "int_equal","type": "integer","input": "text","operator": "equal","value": 5}
+		]}
+	]}`
 
-func TestNew(t *testing.T) {
-	//
-	//
-	// err := json.Unmarshal([]byte(rulestr), &rg)
-	// t.Log(err)
-	// t.Log(rg)
+func TestMatch(t *testing.T) {
+	inputs := []struct {
+		title   string
+		dataset string
+		want    bool
+	}{
+		{"dt-01", `{"float_equal":  1.0, "int_equal": 5, "int_greater":  3, "float_greater": 7.7}`, false},
+		{"dt-02", `{"float_equal":  1.2, "int_equal": 5, "int_greater":  3, "float_greater": 7.7}`, true},
+		{"dt-03", `{"float_equal":  1.2}`, false},
+		{"dt-04", `{"int_greater":  3}`, false},
+	}
+
+	qb := New(parseJson(rulesetStr))
+
+	for _, input := range inputs {
+		t.Run(input.title, func(t *testing.T) {
+			if got := qb.Match(parseJson(input.dataset)); got != input.want {
+				t.Errorf("Match got %t, want %t", got, input.want)
+			}
+		})
+	}
 }
