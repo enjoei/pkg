8295318b794e5d2316146ca587ff8ec5ed0dee33
Leandro Nunes <leandronunes.dev@gmail.com>
2020-01-02 15:38:34 -0300

    add contains operator

---
+++ querybuilder/operator/contains.go
@@ -0,0 +1,34 @@
+package operator
+
+import (
+	"reflect"
+	"strings"
+)
+
+func init() {
+	AddOperator(Contains)
+}
+
+var Contains = &Operator{
+	Name: "contains",
+	Evaluate: func(input, value interface{}) bool {
+		rinput := reflect.ValueOf(input)
+
+		switch rinput.Kind() {
+		case reflect.String:
+			if strings.Contains(input.(string), value.(string)) {
+				return true
+			}
+		case reflect.Slice:
+			for i := 0; i < rinput.Len(); i++ {
+				if rinput.Index(i).Interface() == value {
+					return true
+				}
+			}
+		default:
+			return false
+		}
+
+		return false
+	},
+}
